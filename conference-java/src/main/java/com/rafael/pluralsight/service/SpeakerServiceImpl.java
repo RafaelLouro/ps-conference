4fc135d70a432dae7c745d442c6282672b863b7a
Rafael Louro <rlourox@gmail.com>
2020-03-06 11:07:39 -0300

    Exemplos de BeanFactory, SpEL e Spring Profile

--- conference-java/src/main/java/com/rafael/pluralsight/service/SpeakerServiceImpl.java
+++ conference-java/src/main/java/com/rafael/pluralsight/service/SpeakerServiceImpl.java
@@ -5,30 +5,36 @@ import com.rafael.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
-  private SpeakerRepository repository;
+    private SpeakerRepository repository;
 
-  public SpeakerServiceImpl() {
-    System.out.println("SpeakerServiceImpl no args");
-  }
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args");
+    }
 
-  @Autowired
-  public SpeakerServiceImpl(SpeakerRepository repository) {
-    System.out.println("SpeakerServiceImpl with args");
-    this.repository = repository;
-  }
+    @Autowired
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl with args");
+        this.repository = repository;
+    }
 
-  @Override
-  public List<Speaker> findAll() {
-    return repository.findAll();
-  }
+    @PostConstruct
+    private void initialize() {
+        System.out.println("It's called after the constructor!");
+    }
 
-  public void setRepository(SpeakerRepository repository) {
-    System.out.println("SpeakerServiceImpl setter");
-    this.repository = repository;
-  }
+    @Override
+    public List<Speaker> findAll() {
+        return repository.findAll();
+    }
+
+    public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
+        this.repository = repository;
+    }
 }
